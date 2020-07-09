8a08f3642dab307d915336a87ea6386d707ca678
Kyle Jordaan <kyle.jordaan@takealot.com>
2020-07-09 15:12:29 +0200

    using working hostGenerator

--- init.sh
+++ init.sh
@@ -6,6 +6,11 @@
 sleep 5
 echo "sshd started pid=$(ps auwx |grep [s]sh |  awk '{print $2}')"
 
+/hostGenerator.sh 2>&1 &
+echo "started host Generator"
+/start_caesar.sh 2>&1 &
+echo "started caesar"
+
 if [ ! "z$ONE_SHOT" = "z" ]; then
   bash -c "$*"
   return_code=$?
@@ -15,6 +20,5 @@ else
 fi
 echo -n "$return_code" > /dev/termination-log
 echo -n "$return_code" > $GUILLOTINE/execute
-/kube-openmpi/utils/hostGenerator.sh 2>&1 &
-/start_caesar.sh 2>&1 &
+
 exit $return_code
